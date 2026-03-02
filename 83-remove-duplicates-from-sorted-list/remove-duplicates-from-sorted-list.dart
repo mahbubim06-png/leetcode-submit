ca881d8f8f1994316264de8f1b01fa78a8de8d33
mahbubim06-png <mahbubim06@gmail.com>
2026-03-02 10:59:03 +0530

    Time: 1 ms (52.44%) | Memory: 150.3 MB (52.44%) - LeetSync

---
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.dart
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *   int val;
+ *   ListNode? next;
+ *   ListNode([this.val = 0, this.next]);
+ * }
+ */
+class Solution {
+  ListNode? deleteDuplicates(ListNode? head) {
+    ListNode? current = head;
+
+    while (current != null && current.next != null) {
+      if (current.val == current.next!.val) {
+        current.next = current.next!.next; // remove duplicate
+      } else {
+        current = current.next; // move forward
+      }
+    }
+    return head;
+  }
+}
