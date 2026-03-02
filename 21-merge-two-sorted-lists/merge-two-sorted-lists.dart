1f973ebf9cacdd33c481354f600813a7210d14a3
mahbubim06-png <mahbubim06@gmail.com>
2026-03-02 10:54:55 +0530

    Time: 0 ms (100.00%) | Memory: 148.5 MB (76.97%) - LeetSync

---
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.dart
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *   int val;
+ *   ListNode? next;
+ *   ListNode([this.val = 0, this.next]);
+ * }
+ */
+class Solution {
+  ListNode? mergeTwoLists(ListNode? list1, ListNode? list2) {
+    ListNode dummy = ListNode(0);
+    ListNode current = dummy;
+
+    while (list1 != null && list2 != null) {
+      if (list1.val <= list2.val) {
+        current.next = list1;
+        list1 = list1.next;
+      } else {
+        current.next = list2;
+        list2 = list2.next;
+      }
+      current = current.next!;
+    }
+
+    current.next = list1 ?? list2;
+    return dummy.next;
+  }
+}
